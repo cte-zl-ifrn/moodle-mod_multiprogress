0b5b7703ebffbd602d3965a416b7e76ba8a2678a
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-06-16 17:31:25 -0300

    release: 0.1.13

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_multiprogress';
-$plugin->release = '0.1.12';
-$plugin->version = 2025_06_10_12;
+$plugin->release = '0.1.13';
+$plugin->version = 2025_06_16_13;
 $plugin->requires = 2022_11_28_00;
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = [
