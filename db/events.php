78f1a6ce2c6f479b867a49592435ea5303880eb4
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-07-23 19:50:17 -0300

    fix: observers includefile

--- db/events.php
+++ db/events.php
@@ -30,7 +30,7 @@ $observers = [
     [
         'eventname' => '\core\event\course_module_completion_updated',
         'callback' => '\mod_multiprogress\observers\observers::course_module_completion_updated',
-        'includefile' => '/mod/multiprogress/observers/',
+        'includefile' => '/mod/multiprogress/classes/observers.php',
         'priority' => 200,
         'internal' => true,
     ],
